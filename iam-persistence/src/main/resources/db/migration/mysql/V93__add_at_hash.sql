c67eefa06154103e84fc2983347e4c38d9c09d25
rmiccoli <roberta.miccoli@cnaf.infn.it>
2023-06-09 10:27:34 +0200

    Add db migrations
    - drop UNIQUE constraint on the first 766 characters of the token_value
    - add column to access_token table representing the token_value hash
    - create UNIQUE index on that column

---
+++ iam-persistence/src/main/resources/db/migration/mysql/V93__add_at_hash.sql
@@ -0,0 +1,3 @@
+ALTER TABLE access_token DROP INDEX token_value;
+ALTER TABLE access_token ADD COLUMN token_value_hash CHAR(64) AS HASH('SHA256', token_value);
+ALTER TABLE access_token ADD CONSTRAINT default_token_hash UNIQUE (token_value_hash);
