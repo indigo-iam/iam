84cfad944e2bea40259277f6e9781976f2b963cb
rmiccoli <roberta.miccoli@cnaf.infn.it>
2023-06-16 17:44:45 +0200

    Fix index name in h2 database migration

--- iam-persistence/src/main/resources/db/migration/h2/V93__add_at_hash.sql
+++ iam-persistence/src/main/resources/db/migration/h2/V93__add_at_hash.sql
@@ -1,2 +1,2 @@
 ALTER TABLE access_token ADD COLUMN token_value_hash CHAR(64) AS HASH('SHA256', token_value);
-ALTER TABLE access_token ADD CONSTRAINT default_token_hash UNIQUE (token_value_hash);
+ALTER TABLE access_token ADD CONSTRAINT at_tvh_idx UNIQUE (token_value_hash);
