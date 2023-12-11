a05178ddd59781c46d721a12e3489739bf8eb90e
Davide Marcato <davide.marcato@lnl.infn.it>
2023-12-11 18:32:10 +0100

    Updating client last_used when generating access token

--- iam-login-service/src/main/java/it/infn/mw/iam/core/IamTokenService.java
+++ iam-login-service/src/main/java/it/infn/mw/iam/core/IamTokenService.java
@@ -25,6 +25,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Service;
 
 import com.google.common.collect.Sets;
@@ -76,4 +77,13 @@ public class IamTokenService extends DefaultOAuth2ProviderTokenService {
     refreshTokenRepo.delete(refreshToken);
   }
 
+  @Override
+  public OAuth2AccessTokenEntity createAccessToken(OAuth2Authentication authentication) {
+
+    OAuth2AccessTokenEntity accessToken = super.createAccessToken(authentication);
+
+    accessToken.getClient().setLastUsed(new Date());
+
+    return accessToken;
+  }
 }
