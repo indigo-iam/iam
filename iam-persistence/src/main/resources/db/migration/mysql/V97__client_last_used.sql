116a61f5fdd81a7efd1fe798fb5a748c0472bc5e
Davide Marcato <davide.marcato@lnl.infn.it>
2024-01-24 17:58:53 +0100

    Add missing mysql migration

--- iam-persistence/src/main/resources/db/migration/mysql/V97__client_last_used.sql
+++ iam-persistence/src/main/resources/db/migration/mysql/V97__client_last_used.sql
@@ -1,2 +1,8 @@
 
-ALTER TABLE client_details ADD last_used DATE DEFAULT NULL;
+ALTER TABLE client_details ADD last_used BIGINT DEFAULT NULL;
+
+CREATE TABLE IF NOT EXISTS iam_client_last_used (
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  last_used DATE NOT NULL);
+
+ALTER TABLE client_details ADD CONSTRAINT fk_iam_client_last_used FOREIGN KEY (last_used) REFERENCES iam_client_last_used(id);
