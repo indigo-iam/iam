392eb8469c657c90a319a009fa6e63054526bb3a
Davide Marcato <davide.marcato@lnl.infn.it>
2024-02-02 13:57:52 +0100

    Remove unused import

--- iam-login-service/src/main/java/it/infn/mw/iam/api/client/service/DefaultClientService.java
+++ iam-login-service/src/main/java/it/infn/mw/iam/api/client/service/DefaultClientService.java
@@ -28,7 +28,6 @@ import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.oauth2.common.exceptions.InvalidClientException;
 import org.springframework.security.oauth2.provider.OAuth2RequestValidator;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
