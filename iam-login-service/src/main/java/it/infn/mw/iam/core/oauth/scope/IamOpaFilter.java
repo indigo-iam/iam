bdafb12e02a879b13b4c05c9269a0a2a1ab9ded2
rmiccoli <roberta.miccoli@cnaf.infn.it>
2024-02-21 16:26:51 +0100

    Add license and format code

--- iam-login-service/src/main/java/it/infn/mw/iam/core/oauth/scope/IamOpaFilter.java
+++ iam-login-service/src/main/java/it/infn/mw/iam/core/oauth/scope/IamOpaFilter.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) Istituto Nazionale di Fisica Nucleare (INFN). 2016-2021
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package it.infn.mw.iam.core.oauth.scope;
 
 import java.util.Arrays;
@@ -21,24 +36,24 @@ import it.infn.mw.iam.persistence.model.IamAccount;
 @SuppressWarnings("deprecation")
 @Component
 public class IamOpaFilter {
-  
+
   @Autowired
   private IamOpaController opaService;
-  
-  public void filterScopesWithOPA(OAuth2Authentication authentication, Optional<IamAccount> maybeAccount, OAuth2AccessToken accessToken) {
-    
+
+  public void filterScopesWithOPA(OAuth2Authentication authentication,
+      Optional<IamAccount> maybeAccount, OAuth2AccessToken accessToken) {
+
     OAuth2Request originalAuthRequest = authentication.getOAuth2Request();
-    
+
     JSONObject input = new JSONObject();
     Set<String> filteredScopes = new HashSet<>();
 
     if (maybeAccount.isPresent()) {
-      input.put("id",  maybeAccount.get().getUuid());
+      input.put("id", maybeAccount.get().getUuid());
       input.put("type", "account");
       input.put("scopes", originalAuthRequest.getScope());
-    }
-    else {
-      input.put("id",  originalAuthRequest.getClientId());
+    } else {
+      input.put("id", originalAuthRequest.getClientId());
       input.put("type", "client");
       input.put("scopes", originalAuthRequest.getScope());
     }
@@ -56,7 +71,7 @@ public class IamOpaFilter {
       // TODO Auto-generated catch block
       e.printStackTrace();
     }
-    
+
   }
 
 }
--- iam-login-service/src/main/java/it/infn/mw/iam/core/oauth/scope/IamOpaScopeFilter.java
+++ iam-login-service/src/main/java/it/infn/mw/iam/core/oauth/scope/IamOpaScopeFilter.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) Istituto Nazionale di Fisica Nucleare (INFN). 2016-2021
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package it.infn.mw.iam.core.oauth.scope;
 
 import java.util.Arrays;
@@ -16,15 +31,15 @@ import it.infn.mw.iam.persistence.model.IamAccount;
 
 @Component
 public class IamOpaScopeFilter {
-  
+
   @Autowired
   private IamOpaController opaService;
-  
+
   public Set<String> opaScopeFilter(IamAccount account, Set<String> scopes) {
     JSONObject input = new JSONObject();
     Set<String> filteredScopes = new HashSet<>();
 
-    input.put("id",  account.getUuid());
+    input.put("id", account.getUuid());
     input.put("type", "account");
     input.put("scopes", scopes);
 
