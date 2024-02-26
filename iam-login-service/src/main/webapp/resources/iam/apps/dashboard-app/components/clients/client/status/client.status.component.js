09b64b0ea6c32d4e71f3e93599770c3d08040d36
Manoj Garai <manoj.garai@stfc.ac.uk>
2024-02-26 16:21:27 +0000

    Add client status component
    Based on the active status of client, disable and restore button will toggle the status

---
+++ iam-login-service/src/main/webapp/resources/iam/apps/dashboard-app/components/clients/client/status/client.status.component.js
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) Istituto Nazionale di Fisica Nucleare (INFN). 2016-2021
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+(function() {
+  'use strict';
+
+  function ClientStatusController(toaster, ModalService, ClientsService) {
+    var self = this;
+
+    self.$onInit = function() {
+      self.enabled = true;
+    };
+
+    self.handleError = function(error) {
+      self.clientCtrl.handleError(error);
+      self.enabled = true;
+    };
+
+    self.handleSuccess = function() {
+      self.enabled = true;
+      self.clientCtrl.loadClient().then(function(client) {
+        if (client.active) {
+          toaster.pop({
+            type: 'success',
+            body:
+                `Client '${client.client_name}' has been restored successfully.`
+          });
+        } else {
+          toaster.pop({
+            type: 'success',
+            body: `Client '${client.client_name}' is now disabled.`
+          });
+        }
+      });
+    };
+
+    self.enableClient = function() {
+      return ClientsService.setClientActiveStatus(self.client.client_id, true)
+          .then(self.handleSuccess)
+          .catch(self.handleError);
+    };
+
+    self.disableClient = function() {
+      return ClientsService.setClientActiveStatus(self.client.client_id, false)
+          .then(self.handleSuccess)
+          .catch(self.handleError);
+    };
+
+
+    self.openDialog = function() {
+
+      var modalOptions = null;
+      var updateStatusFunc = null;
+
+      if (self.client.active) {
+        modalOptions = {
+          closeButtonText: 'Cancel',
+          actionButtonText: 'Disable client',
+          headerText: 'Disable ' + self.client.client_name,
+          bodyText:
+              `Are you sure you want to disable client '${self.client.client_name}'?`
+        };
+        updateStatusFunc = self.disableClient;
+
+      } else {
+        modalOptions = {
+          closeButtonText: 'Cancel',
+          actionButtonText: 'Restore client',
+          headerText: 'Restore ' + self.client.client_name,
+          bodyText:
+              `Are you sure you want to restore client '${self.client.client_name}'?`
+        };
+        updateStatusFunc = self.enableClient;
+      }
+
+      self.enable = false;
+      ModalService.showModal({}, modalOptions)
+          .then(function() { updateStatusFunc(); })
+          .catch(function() {
+
+          });
+    };
+  }
+
+  angular.module('dashboardApp').component('clientStatus', {
+    require: {clientCtrl: '^client'},
+    bindings: {client: '='},
+    templateUrl:
+        '/resources/iam/apps/dashboard-app/components/clients/client/status/client.status.component.html',
+    controller: [
+      'toaster', 'ModalService', 'ClientsService', ClientStatusController
+    ]
+  });
+
+})();
