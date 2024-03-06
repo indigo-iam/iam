316c56269804d42268c1091408ff2b24f44ea645
Davide Marcato <davide.marcato@lnl.infn.it>
2024-03-06 11:31:18 +0100

    First PoC on docker compose

---
+++ compose/voms-replica/assets/db-replica/replicate.sql
@@ -0,0 +1,11 @@
+STOP SLAVE;
+CHANGE REPLICATION SOURCE TO 
+  SOURCE_HOST='db-primary.test.example',
+  SOURCE_USER='replicator',
+  SOURCE_PASSWORD='pwd',
+  SOURCE_SSL=1,
+  SOURCE_SSL_CA = '/certs/ca-cert.pem',
+  SOURCE_SSL_CERT = '/certs/client-cert.pem',
+  SOURCE_SSL_KEY = '/certs/client-key.pem',
+  SOURCE_SSL_VERIFY_SERVER_CERT=1;
+START SLAVE;
