cc15afc14b15545708ddeab9c270cf14548c6c90
Davide Marcato <davide.marcato@lnl.infn.it>
2024-03-15 14:31:16 +0100

    Remove manual CA

--- compose/voms-replica/assets/db-replica/replicate.sql
+++ compose/voms-replica/assets/db-replica/replicate.sql
@@ -4,6 +4,6 @@ CHANGE REPLICATION SOURCE TO
   SOURCE_USER='replicator',
   SOURCE_PASSWORD='pwd',
   SOURCE_SSL=1,
-  SOURCE_SSL_CA = '/certs/ca-cert.pem',
+  SOURCE_SSL_CA = '/etc/grid-security/certificates/igi-test-ca.pem',
   SOURCE_SSL_VERIFY_SERVER_CERT=1;
 START REPLICA;
