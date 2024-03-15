7bae731aec9e65c31a4d6556cea0184b8c00c930
Manoj Garai <manoj.garai@stfc.ac.uk>
2024-03-15 10:06:14 +0000

    Add default date to status changed on

--- iam-persistence/src/main/resources/db/migration/mysql/V102__add_active_and_status_changed_on_to_client_details.sql
+++ iam-persistence/src/main/resources/db/migration/mysql/V102__add_active_and_status_changed_on_to_client_details.sql
@@ -1,1 +1,1 @@
-ALTER TABLE client_details ADD COLUMN (active BOOLEAN, status_changed_on TIMESTAMP);
+ALTER TABLE client_details ADD COLUMN (active BOOLEAN, status_changed_on TIMESTAMP DEFAULT '1970-01-01 00:00:01');
