d12638af4c0d9ed7c84bd9fbfb3f09ea881ef137
rmiccoli <roberta.miccoli@cnaf.infn.it>
2024-03-18 11:01:59 +0100

    Fix test

--- iam-login-service/src/test/java/it/infn/mw/iam/test/multi_factor_authentication/IamTotpMfaServiceTests.java
+++ iam-login-service/src/test/java/it/infn/mw/iam/test/multi_factor_authentication/IamTotpMfaServiceTests.java
@@ -18,12 +18,10 @@ package it.infn.mw.iam.test.multi_factor_authentication;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.times;
@@ -31,7 +29,6 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
-import java.util.Set;
 
 import org.junit.After;
 import org.junit.Before;
@@ -45,7 +42,6 @@ import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationEventPublisher;
 
 import dev.samstevens.totp.code.CodeVerifier;
-import dev.samstevens.totp.recovery.RecoveryCodeGenerator;
 import dev.samstevens.totp.secret.SecretGenerator;
 import it.infn.mw.iam.api.account.multi_factor_authentication.DefaultIamTotpMfaService;
 import it.infn.mw.iam.api.account.multi_factor_authentication.IamTotpMfaService;
@@ -58,7 +54,6 @@ import it.infn.mw.iam.core.user.exception.MfaSecretNotFoundException;
 import it.infn.mw.iam.core.user.exception.TotpMfaAlreadyEnabledException;
 import it.infn.mw.iam.persistence.model.IamAccount;
 import it.infn.mw.iam.persistence.model.IamTotpMfa;
-import it.infn.mw.iam.persistence.model.IamTotpRecoveryCode;
 import it.infn.mw.iam.persistence.repository.IamTotpMfaRepository;
 import it.infn.mw.iam.util.mfa.IamTotpMfaEncryptionAndDecryptionUtil;
 import it.infn.mw.iam.util.mfa.IamTotpMfaInvalidArgumentError;
@@ -73,9 +68,6 @@ public class IamTotpMfaServiceTests extends IamTotpMfaServiceTestSupport {
 
   @Mock
   private SecretGenerator secretGenerator;
-
-  @Mock
-  private RecoveryCodeGenerator recoveryCodeGenerator;
 
   @Mock
   private IamAccountService iamAccountService;
@@ -101,18 +93,13 @@ public class IamTotpMfaServiceTests extends IamTotpMfaServiceTestSupport {
     when(iamAccountService.saveAccount(TOTP_MFA_ACCOUNT)).thenAnswer(i -> i.getArguments()[0]);
     when(codeVerifier.isValidCode(anyString(), anyString())).thenReturn(true);
 
-    String[] testArray = { TOTP_RECOVERY_CODE_STRING_7, TOTP_RECOVERY_CODE_STRING_8,
-        TOTP_RECOVERY_CODE_STRING_9, TOTP_RECOVERY_CODE_STRING_10, TOTP_RECOVERY_CODE_STRING_11,
-        TOTP_RECOVERY_CODE_STRING_12 };
-    when(recoveryCodeGenerator.generateCodes(anyInt())).thenReturn(testArray);
-
     service = new DefaultIamTotpMfaService(iamAccountService, repository, secretGenerator,
-        recoveryCodeGenerator, codeVerifier, eventPublisher, iamTotpMfaProperties);
+        codeVerifier, eventPublisher, iamTotpMfaProperties);
   }
 
   @After
   public void tearDown() {
-    reset(secretGenerator, repository, iamAccountService, codeVerifier, recoveryCodeGenerator);
+    reset(secretGenerator, repository, iamAccountService, codeVerifier);
   }
 
   @Test
@@ -123,7 +110,6 @@ public class IamTotpMfaServiceTests extends IamTotpMfaServiceTestSupport {
     IamTotpMfa totpMfa = service.addTotpMfaSecret(account);
     verify(repository, times(1)).save(totpMfa);
     verify(secretGenerator, times(1)).generate();
-    verify(recoveryCodeGenerator, times(1)).generateCodes(anyInt());
 
     assertNotNull(totpMfa.getSecret());
     assertFalse(totpMfa.isActive());
@@ -144,61 +130,17 @@ public class IamTotpMfaServiceTests extends IamTotpMfaServiceTestSupport {
   }
 
   @Test
-  public void testAddsMfaRecoveryCodesToAccount() {
-    IamAccount account = cloneAccount(TOTP_MFA_ACCOUNT);
-    IamTotpMfa totpMfa = cloneTotpMfa(TOTP_MFA);
-    Set<IamTotpRecoveryCode> originalCodes = totpMfa.getRecoveryCodes();
-
-    try {
-      totpMfa = service.addTotpMfaRecoveryCodes(account);
-    } catch (MfaSecretNotFoundException e) {
-      assertThat(e.getMessage(), equalTo("No multi-factor secret is attached to this account"));
-      throw e;
-    }
-
-    Set<IamTotpRecoveryCode> newCodes = totpMfa.getRecoveryCodes();
-    assertThat(originalCodes.toArray(), not(equalTo(newCodes.toArray())));
-  }
-
-  @Test(expected = MfaSecretNotFoundException.class)
-  public void testAddsMfaRecoveryCode_whenNoMfaSecretAssignedFails() {
-    when(repository.findByAccount(TOTP_MFA_ACCOUNT)).thenReturn(Optional.empty());
-
-    IamAccount account = cloneAccount(TOTP_MFA_ACCOUNT);
-
-    try {
-      service.addTotpMfaRecoveryCodes(account);
-    } catch (MfaSecretNotFoundException e) {
-      assertThat(e.getMessage(), equalTo("No multi-factor secret is attached to this account"));
-      throw e;
-    }
-  }
-
-  @Test
   public void testAddTotpMfaSecret_whenPasswordIsEmpty() {
     when(repository.findByAccount(TOTP_MFA_ACCOUNT)).thenReturn(Optional.empty());
     when(iamTotpMfaProperties.getPasswordToEncryptOrDecrypt()).thenReturn("");
 
     IamAccount account = cloneAccount(TOTP_MFA_ACCOUNT);
 
-    IamTotpMfaInvalidArgumentError thrownException = assertThrows(IamTotpMfaInvalidArgumentError.class, () -> {
-      // Decrypt the cipherText with empty key
-      service.addTotpMfaSecret(account);
-    });
-
-    assertTrue(thrownException.getMessage().startsWith("Please ensure that you provide"));
-  }
-
-  @Test
-  public void testAddsMfaRecoveryCodes_whenPasswordIsEmpty() {
-    when(iamTotpMfaProperties.getPasswordToEncryptOrDecrypt()).thenReturn("");
-
-    IamAccount account = cloneAccount(TOTP_MFA_ACCOUNT);
-
-    IamTotpMfaInvalidArgumentError thrownException = assertThrows(IamTotpMfaInvalidArgumentError.class, () -> {
-      // Decrypt the cipherText with empty key
-      service.addTotpMfaRecoveryCodes(account);
-    });
+    IamTotpMfaInvalidArgumentError thrownException =
+        assertThrows(IamTotpMfaInvalidArgumentError.class, () -> {
+          // Decrypt the cipherText with empty key
+          service.addTotpMfaSecret(account);
+        });
 
     assertTrue(thrownException.getMessage().startsWith("Please ensure that you provide"));
   }
@@ -207,9 +149,8 @@ public class IamTotpMfaServiceTests extends IamTotpMfaServiceTestSupport {
   public void testEnablesTotpMfa() throws Exception {
     IamAccount account = cloneAccount(TOTP_MFA_ACCOUNT);
     IamTotpMfa totpMfa = cloneTotpMfa(TOTP_MFA);
-    totpMfa.setSecret(
-        IamTotpMfaEncryptionAndDecryptionUtil.encryptSecretOrRecoveryCode(
-            "secret", iamTotpMfaProperties.getPasswordToEncryptOrDecrypt()));
+    totpMfa.setSecret(IamTotpMfaEncryptionAndDecryptionUtil.encryptSecretOrRecoveryCode("secret",
+        iamTotpMfaProperties.getPasswordToEncryptOrDecrypt()));
     totpMfa.setActive(false);
     totpMfa.setAccount(account);
 
@@ -290,9 +231,10 @@ public class IamTotpMfaServiceTests extends IamTotpMfaServiceTestSupport {
 
     IamAccount account = cloneAccount(TOTP_MFA_ACCOUNT);
 
-    MfaSecretNotFoundException thrownException = assertThrows(MfaSecretNotFoundException.class, () -> {
-      service.verifyTotp(account, TOTP_CODE);
-    });
+    MfaSecretNotFoundException thrownException =
+        assertThrows(MfaSecretNotFoundException.class, () -> {
+          service.verifyTotp(account, TOTP_CODE);
+        });
 
     assertTrue(thrownException.getMessage().startsWith("No multi-factor secret is attached"));
   }
@@ -317,9 +259,10 @@ public class IamTotpMfaServiceTests extends IamTotpMfaServiceTestSupport {
 
     IamAccount account = cloneAccount(TOTP_MFA_ACCOUNT);
 
-    IamTotpMfaInvalidArgumentError thrownException = assertThrows(IamTotpMfaInvalidArgumentError.class, () -> {
-      service.verifyTotp(account, TOTP_CODE);
-    });
+    IamTotpMfaInvalidArgumentError thrownException =
+        assertThrows(IamTotpMfaInvalidArgumentError.class, () -> {
+          service.verifyTotp(account, TOTP_CODE);
+        });
 
     assertTrue(thrownException.getMessage().startsWith("Please ensure that you provide"));
   }
@@ -336,42 +279,4 @@ public class IamTotpMfaServiceTests extends IamTotpMfaServiceTestSupport {
     assertFalse(service.verifyTotp(account, TOTP_CODE));
   }
 
-  @Test
-  public void verifyRecoveryCode_WithNoMultiFactorSecretAttached() {
-    when(repository.findByAccount(TOTP_MFA_ACCOUNT)).thenReturn(Optional.empty());
-
-    IamAccount account = cloneAccount(TOTP_MFA_ACCOUNT);
-
-    MfaSecretNotFoundException thrownException = assertThrows(MfaSecretNotFoundException.class, () -> {
-      service.verifyRecoveryCode(account, TOTP_RECOVERY_CODE_STRING_1);
-    });
-
-    assertTrue(thrownException.getMessage().startsWith("No multi-factor secret is attached"));
-  }
-
-  @Test
-  public void verifyRecoveryCode() {
-    IamTotpMfa totpMfa = cloneTotpMfa(TOTP_MFA);
-
-    when(repository.findByAccount(TOTP_MFA_ACCOUNT)).thenReturn(Optional.of(totpMfa));
-
-    IamAccount account = cloneAccount(TOTP_MFA_ACCOUNT);
-
-    assertTrue(service.verifyRecoveryCode(account, TOTP_RECOVERY_CODE_STRING_1));
-  }
-
-  @Test
-  public void verifyRecoveryCode_WithEmptyPasswordForDecryption() {
-    IamTotpMfa totpMfa = cloneTotpMfa(TOTP_MFA);
-
-    when(repository.findByAccount(TOTP_MFA_ACCOUNT)).thenReturn(Optional.of(totpMfa));
-    when(iamTotpMfaProperties.getPasswordToEncryptOrDecrypt()).thenReturn("");
-
-    IamAccount account = cloneAccount(TOTP_MFA_ACCOUNT);
-    IamTotpMfaInvalidArgumentError thrownException = assertThrows(IamTotpMfaInvalidArgumentError.class, () -> {
-      service.verifyRecoveryCode(account, TOTP_RECOVERY_CODE_STRING_1);
-    });
-
-    assertTrue(thrownException.getMessage().startsWith("Please ensure that you provide"));
-  }
 }
