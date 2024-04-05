e430264c95caf02c87e4c525aaa258b4a9c9bcb4
Manoj Garai <manoj.garai@stfc.ac.uk>
2024-04-05 13:19:43 +0100

    Add column status_changed_by to client_details

--- iam-persistence/src/main/resources/db/migration/mysql/V102__add_active_and_status_changed_on_to_client_details.sql
+++ iam-persistence/src/main/resources/db/migration/mysql/V102__add_active_and_status_changed_on_to_client_details.sql
@@ -1,1 +1,3 @@
-ALTER TABLE client_details ADD COLUMN (active BOOLEAN, status_changed_on TIMESTAMP DEFAULT '1970-01-01 00:00:01');
+ALTER TABLE client_details ADD COLUMN (active BOOLEAN, 
+                                        status_changed_on TIMESTAMP DEFAULT '1970-01-01 00:00:01',
+                                        status_changed_by VARCHAR(36));
