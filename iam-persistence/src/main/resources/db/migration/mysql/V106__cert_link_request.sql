0f89ff2ae41a0b89841535412a538e24e98fff5f
Davide Marcato <davide.marcato@lnl.infn.it>
2024-07-26 13:52:51 +0200

    Fix migration

--- iam-persistence/src/main/resources/db/migration/mysql/V106__cert_link_request.sql
+++ iam-persistence/src/main/resources/db/migration/mysql/V106__cert_link_request.sql
@@ -10,4 +10,4 @@ CREATE TABLE iam_cert_link_request (
     LASTUPDATETIME TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP);
 
 ALTER TABLE iam_cert_link_request ADD CONSTRAINT FK_iam_cert_link_request_account_id FOREIGN KEY (ACCOUNT_ID) REFERENCES iam_account (ID);
-ALTER TABLE iam_cert_link_request ADD CONSTRAINT FK_iam_cert_link_request_x509_cert_id FOREIGN KEY (IAM_X509_CERT_ID) REFERENCES IAM_X509_CERT (ID);
+ALTER TABLE iam_cert_link_request ADD CONSTRAINT FK_iam_cert_link_request_x509_cert_id FOREIGN KEY (IAM_X509_CERT_ID) REFERENCES iam_x509_cert (ID);
