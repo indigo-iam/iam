e7caf1cfff8efc21d9338407f4c7518c9c6d5178
rmiccoli <roberta.miccoli@cnaf.infn.it>
2024-08-02 19:44:52 +0200

    Change test

--- iam-login-service/src/test/java/it/infn/mw/iam/test/api/aup/AupReminderTaskTests.java
+++ iam-login-service/src/test/java/it/infn/mw/iam/test/api/aup/AupReminderTaskTests.java
@@ -22,6 +22,7 @@ import static org.hamcrest.Matchers.is;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.Test;
@@ -44,6 +45,7 @@ import it.infn.mw.iam.persistence.repository.IamEmailNotificationRepository;
 import it.infn.mw.iam.service.aup.DefaultAupSignatureCheckService;
 import it.infn.mw.iam.test.core.CoreControllerTestSupport;
 import it.infn.mw.iam.test.notification.NotificationTestConfig;
+import it.infn.mw.iam.test.util.MockTimeProvider;
 import it.infn.mw.iam.test.util.WithAnonymousUser;
 import it.infn.mw.iam.test.util.annotation.IamMockMvcIntegrationTest;
 import it.infn.mw.iam.test.util.notification.MockNotificationDelivery;
@@ -77,6 +79,9 @@ public class AupReminderTaskTests extends AupTestSupport {
   @Autowired
   private IamAupRepository aupRepo;
 
+  @Autowired
+  private MockTimeProvider mockTimeProvider;
+
   @After
   public void tearDown() {
     notificationDelivery.clearDeliveredNotifications();
@@ -85,26 +90,30 @@ public class AupReminderTaskTests extends AupTestSupport {
 
   @Test
   @WithMockUser(username = "admin", roles = {"ADMIN", "USER"})
-  public void aupReminderEmailWorks() throws InterruptedException {
+  public void aupReminderEmailWorks() {
     IamAup aup = buildDefaultAup();
     aup.setSignatureValidityInDays(30L);
     aupRepo.save(aup);
 
     Date now = new Date();
+    mockTimeProvider.setTime(now.getTime());
     LocalDate today = LocalDate.now();
     LocalDate tomorrow = today.plusDays(1);
     Date tomorrowDate = Date.from(tomorrow.atStartOfDay(ZoneId.systemDefault()).toInstant());
 
-    IamAccount testAccount = accountRepo.findByUsername("test_100")
+    IamAccount testAccount = accountRepo.findByUsername("test")
       .orElseThrow(() -> new AssertionError("Expected test account not found"));
+
+    mockTimeProvider.setTime(now.getTime() + TimeUnit.MINUTES.toMillis(5));
 
     assertThat(service.needsAupSignature(testAccount), is(true));
 
-    signatureRepo.createSignatureForAccount(aup, testAccount, now);
-
-    Thread.sleep(5000);
+    signatureRepo.createSignatureForAccount(aup, testAccount,
+        new Date(mockTimeProvider.currentTimeMillis()));
 
     assertThat(service.needsAupSignature(testAccount), is(false));
+
+    mockTimeProvider.setTime(now.getTime() + TimeUnit.MINUTES.toMillis(10));
 
     assertThat(notificationRepo.countAupRemindersPerAccount(testAccount.getUserInfo().getEmail(),
         tomorrowDate), equalTo(0));
