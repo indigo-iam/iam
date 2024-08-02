0e299160a1df744850bcefacf8bd1dc01c508209
Stefano <SteDev2@users.noreply.github.com>
2024-08-02 15:17:23 +0200

    Improve password quality check (#719)
    * Use stronger REGEX for password change or reset
    * Add reset token validation
    * Fix event publication

---
+++ iam-login-service/src/main/java/it/infn/mw/iam/util/RegexUtil.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) Istituto Nazionale di Fisica Nucleare (INFN). 2016-2021
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package it.infn.mw.iam.util;
+
+public final class RegexUtil {
+
+  private RegexUtil() {}
+
+  // Regex matches password with at least one lowercase letter, one uppercase
+  // letter, one number, one symbol and minimum length of 8 characters
+  public static final String PASSWORD_REGEX =
+      "^(?=.*[\\p{Lower}])(?=.*[\\p{Upper}])(?=.*[\\p{Digit}])(?=.*[\\p{Punct}]).{8,}([^\\r\\t\\v\\f\\n]+)$";
+  public static final String PASSWORD_REGEX_MESSAGE_ERROR =
+      "The password must include at least one uppercase letter, one lowercase letter, one number, one symbol (e.g., @$!%*?&) and must contain at least 8 characters for greater security.";
+}
