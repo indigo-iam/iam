1aa1efde696310b9d3ea36e5d17926673a127120
garaimanoj <manoj.garai@stfc.ac.uk>
2024-08-16 12:15:07 +0000

    Delete unnecessary tests

--- iam-login-service/src/test/java/it/infn/mw/iam/test/notification/ServiceAccountNotificationTest.java
+++ iam-login-service/src/test/java/it/infn/mw/iam/test/notification/ServiceAccountNotificationTest.java
@@ -13,19 +13,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package it.infn.mw.iam.test.notification;
 
-package it.infn.mw.iam.test.api.scim.controller;
-
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
-
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 
 import org.junit.After;
 import org.junit.Before;
@@ -43,22 +42,19 @@ import org.springframework.web.context.WebApplicationContext;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import it.infn.mw.iam.test.util.WithAnonymousUser;
-import it.infn.mw.iam.test.util.annotation.IamMockMvcIntegrationTest;
-import it.infn.mw.iam.test.util.notification.MockNotificationDelivery;
-import it.infn.mw.iam.test.util.oauth.MockOAuth2Filter;
+import it.infn.mw.iam.IamLoginService;
 import it.infn.mw.iam.api.scim.model.ScimUser;
 import it.infn.mw.iam.api.scim.model.ScimUserPatchRequest;
 import it.infn.mw.iam.core.IamNotificationType;
-import it.infn.mw.iam.IamLoginService;
-import it.infn.mw.iam.api.scim.model.ScimConstants;
 import it.infn.mw.iam.persistence.model.IamAccount;
 import it.infn.mw.iam.persistence.model.IamEmailNotification;
 import it.infn.mw.iam.persistence.repository.IamAccountRepository;
 import it.infn.mw.iam.persistence.repository.IamEmailNotificationRepository;
-import it.infn.mw.iam.test.api.TestSupport;
 import it.infn.mw.iam.test.core.CoreControllerTestSupport;
-import it.infn.mw.iam.test.notification.NotificationTestConfig;
+import it.infn.mw.iam.test.util.WithAnonymousUser;
+import it.infn.mw.iam.test.util.annotation.IamMockMvcIntegrationTest;
+import it.infn.mw.iam.test.util.notification.MockNotificationDelivery;
+import it.infn.mw.iam.test.util.oauth.MockOAuth2Filter;
 
 @RunWith(SpringRunner.class)
 @IamMockMvcIntegrationTest
@@ -66,38 +62,43 @@ import it.infn.mw.iam.test.notification.NotificationTestConfig;
         NotificationTestConfig.class }, webEnvironment = WebEnvironment.MOCK)
 @WithAnonymousUser
 @TestPropertySource(properties = { "notification.disable=false" })
-public class ScimUserControllerTest extends TestSupport implements ScimConstants {
-    
-    @Autowired
-    private WebApplicationContext context;
-    @Autowired
-    private MockOAuth2Filter mockOAuth2Filter;
-    @Autowired
-    private IamAccountRepository iamAccountRepo;
-    @Autowired
-    private ObjectMapper mapper;
-    @Autowired
-    private IamEmailNotificationRepository notificationRepo;
+public class ServiceAccountNotificationTest {
+
     @Autowired
     private MockNotificationDelivery notificationDelivery;
+
+    @Autowired
+    private MockOAuth2Filter mockOAuth2Filter;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Autowired
+    private IamAccountRepository iamAccountRepo;
+
+    @Autowired
+    private IamEmailNotificationRepository notificationRepo;
 
     private MockMvc mvc;
 
     @Before
-    public void setup() {
+    public void setUp() throws InterruptedException {
         mvc = MockMvcBuilders.webAppContextSetup(context).alwaysDo(log()).apply(springSecurity()).build();
-        mockOAuth2Filter.cleanupSecurityContext();
     }
 
     @After
-    public void cleanupOAuthUser() {
+    public void tearDown() throws InterruptedException {
         mockOAuth2Filter.cleanupSecurityContext();
+        notificationDelivery.clearDeliveredNotifications();
     }
 
     @Test
     @WithMockUser(username = "admin", roles = { "ADMIN", "USER" })
     public void testEmailSentForSettingServiceAccount() throws Exception {
-        IamAccount testUser = iamAccountRepo.findByUsername(TEST_USER)
+        IamAccount testUser = iamAccountRepo.findByUsername("test")
                 .orElseThrow(() -> new AssertionError("Expected test user not found"));
         ScimUser user = ScimUser.builder().serviceAccount(true).build();
 
@@ -105,8 +106,8 @@ public class ScimUserControllerTest extends TestSupport implements ScimConstants
 
         mvc.perform(patch("/scim/Users/{id}", testUser.getUuid())
                 .content(mapper.writeValueAsString(patchRequest))
-                .contentType(SCIM_CONTENT_TYPE))
-                .andExpect(NO_CONTENT);
+                .contentType("application/scim+json;charset=UTF-8"))
+                .andExpect(status().isNoContent());
         List<IamEmailNotification> notifications = notificationRepo
                 .findByNotificationType(IamNotificationType.SET_SERVICE_ACCOUNT);
 
@@ -130,7 +131,7 @@ public class ScimUserControllerTest extends TestSupport implements ScimConstants
     @Test
     @WithMockUser(username = "admin", roles = { "ADMIN", "USER" })
     public void testEmailSentForRevokingServiceAccount() throws Exception {
-        IamAccount testUser = iamAccountRepo.findByUsername(TEST_USER)
+        IamAccount testUser = iamAccountRepo.findByUsername("test")
                 .orElseThrow(() -> new AssertionError("Expected test user not found"));
         testUser.setServiceAccount(true);
         iamAccountRepo.save(testUser);
@@ -141,8 +142,8 @@ public class ScimUserControllerTest extends TestSupport implements ScimConstants
 
         mvc.perform(patch("/scim/Users/{id}", testUser.getUuid())
                 .content(mapper.writeValueAsString(patchRequest))
-                .contentType(SCIM_CONTENT_TYPE))
-                .andExpect(NO_CONTENT);
+                .contentType("application/scim+json;charset=UTF-8"))
+                .andExpect(status().isNoContent());
         List<IamEmailNotification> notifications = notificationRepo
                 .findByNotificationType(IamNotificationType.REVOKE_SERVICE_ACCOUNT);
 
@@ -162,4 +163,5 @@ public class ScimUserControllerTest extends TestSupport implements ScimConstants
 
         notificationDelivery.clearDeliveredNotifications();
     }
+
 }
