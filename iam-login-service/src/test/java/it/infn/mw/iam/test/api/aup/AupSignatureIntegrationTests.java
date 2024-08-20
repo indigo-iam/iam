ec22e8c21a2ce7c55857f649888121b0d635df0d
garaimanoj <manoj.garai@stfc.ac.uk>
2024-08-20 14:47:26 +0000

    Fix test

--- iam-login-service/src/test/java/it/infn/mw/iam/test/api/aup/AupSignatureIntegrationTests.java
+++ iam-login-service/src/test/java/it/infn/mw/iam/test/api/aup/AupSignatureIntegrationTests.java
@@ -303,7 +303,7 @@ public class AupSignatureIntegrationTests extends AupTestSupport {
     mvc.perform(delete("/iam/aup/signature/" + account.getUuid()))
       .andExpect(status().isNoContent());
     mvc.perform(delete("/iam/aup/signature/" + account.getUuid()))
-      .andExpect(status().isNoContent());
+      .andExpect(status().isNotFound());
     mvc.perform(get("/iam/aup/signature/" + account.getUuid()))
       .andExpect(status().isNotFound())
       .andExpect(jsonPath("$.error", equalTo("AUP signature not found for user 'admin'")));
