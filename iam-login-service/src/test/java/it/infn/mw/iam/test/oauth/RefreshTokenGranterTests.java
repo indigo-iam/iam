a1709cc2a08374854843a5f1ed70245b198eb790
rmiccoli <roberta.miccoli@cnaf.infn.it>
2024-09-09 17:03:51 +0200

    Increase coverage

--- iam-login-service/src/test/java/it/infn/mw/iam/test/oauth/RefreshTokenGranterTests.java
+++ iam-login-service/src/test/java/it/infn/mw/iam/test/oauth/RefreshTokenGranterTests.java
@@ -15,28 +15,41 @@
  */
 package it.infn.mw.iam.test.oauth;
 
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.text.ParseException;
 import java.util.Date;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mitre.oauth2.model.AuthenticationHolderEntity;
 import org.mitre.oauth2.model.ClientDetailsEntity;
+import org.mitre.oauth2.model.OAuth2AccessTokenEntity;
+import org.mitre.oauth2.model.OAuth2RefreshTokenEntity;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Request;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jwt.JWT;
 
 import it.infn.mw.iam.IamLoginService;
 import it.infn.mw.iam.api.client.service.ClientService;
 import it.infn.mw.iam.api.client.util.ClientSuppliers;
+import it.infn.mw.iam.audit.events.tokens.AccessTokenIssuedEvent;
 import it.infn.mw.iam.persistence.model.IamAup;
 import it.infn.mw.iam.persistence.repository.IamAccountRepository;
 import it.infn.mw.iam.persistence.repository.IamAupRepository;
@@ -206,6 +219,32 @@ public class RefreshTokenGranterTests {
 
     client.setActive(true);
     clientService.updateClient(client);
+  }
+
+  @Test
+  public void testRefreshTokenParsingThrowsParseException() throws Exception {
+    OAuth2AccessTokenEntity accessToken = Mockito.mock(OAuth2AccessTokenEntity.class);
+    OAuth2RefreshTokenEntity refreshToken = Mockito.mock(OAuth2RefreshTokenEntity.class);
+    AuthenticationHolderEntity authnHolder = Mockito.mock(AuthenticationHolderEntity.class);
+    OAuth2Authentication authn = Mockito.mock(OAuth2Authentication.class);
+    OAuth2Request req = Mockito.mock(OAuth2Request.class);
+
+    JWT jwt = Mockito.mock(JWT.class);
+
+    when(accessToken.getRefreshToken()).thenReturn(refreshToken);
+    when(accessToken.getAuthenticationHolder()).thenReturn(authnHolder);
+    when(accessToken.getAuthenticationHolder().getAuthentication()).thenReturn(authn);
+    when(authn.getOAuth2Request()).thenReturn(req);
+    when(accessToken.getJwt()).thenReturn(jwt);
+    JWSHeader header = new JWSHeader(JWSAlgorithm.HS256);
+    when(jwt.getHeader()).thenReturn(header);
+    when(accessToken.getRefreshToken().getJwt()).thenReturn(jwt);
+    when(jwt.getJWTClaimsSet()).thenThrow(new ParseException("parse error", 0));
+
+    AccessTokenIssuedEvent event = new AccessTokenIssuedEvent(this, accessToken);
+
+    assertNull(event.getRefreshTokenJti());
+
   }
 
 }
