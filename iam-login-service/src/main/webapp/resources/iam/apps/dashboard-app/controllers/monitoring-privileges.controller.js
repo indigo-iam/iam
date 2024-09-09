24a365ec5b895c11d45bed7ef4802e7914383ea7
garaimanoj <manoj.garai@stfc.ac.uk>
2024-09-09 10:32:11 +0000

    Refactor

--- iam-login-service/src/main/webapp/resources/iam/apps/dashboard-app/controllers/monitoring-privileges.controller.js
+++ iam-login-service/src/main/webapp/resources/iam/apps/dashboard-app/controllers/monitoring-privileges.controller.js
@@ -15,56 +15,51 @@
  */
 'use strict';
 
-angular.module('dashboardApp').controller('MonitoringPrivilegesController',
-	MonitoringPrivilegesController);
+angular.module('dashboardApp').controller('MonitoringPrivilegesController', MonitoringPrivilegesController);
 
-	MonitoringPrivilegesController.$inject= [ '$scope', '$rootScope', '$uibModalInstance', 'Utils',
-                       		'Authorities', 'user'];
+MonitoringPrivilegesController.$inject = ['$scope', '$rootScope', '$uibModalInstance', 'Utils',
+	'Authorities', 'user'];
 
-function MonitoringPrivilegesController($scope, $rootScope, $uibModalInstance, Utils, Authorities, user){
-	
+function MonitoringPrivilegesController($scope, $rootScope, $uibModalInstance, Utils, Authorities, user) {
+
 	var ctrl = this;
 
 	ctrl.user = user;
-	
 	ctrl.enabled = true;
 	ctrl.cancel = cancel;
 	ctrl.assign = assign;
 	ctrl.revoke = revoke;
-	
+
 	ctrl.userName = user.name.formatted;
-	
-	function successHandler(result){
+
+	function successHandler(result) {
 		$uibModalInstance.close(result);
 	}
-	
-	function errorHandler(error){
+
+	function errorHandler(error) {
 		var data = error.data;
-		if (data.error){
+		if (data.error) {
 			console.error('Error:', data.error);
 			$scope.operationResult = Utils.buildErrorResult(data.error);
-		}else{
+		} else {
 			console.error(data);
 			$scope.operationResult = Utils.buildErrorResult(data);
 		}
-		
 		ctrl.enabled = true;
 	}
-	
+
 	function assign() {
 		ctrl.enabled = false;
-				
+
 		Authorities.assignMonitoringPrivileges(ctrl.user.id).then(successHandler, errorHandler);
 	}
-	
+
 	function revoke() {
 		ctrl.enabled = false;
-				
-		Authorities.revokeMonitoringPrivileges(ctrl.user.id).then(
-				successHandler,
-				errorHandler);
+
+		Authorities.revokeMonitoringPrivileges(ctrl.user.id).then(successHandler, errorHandler);
 	}
-	
+
 	function cancel() {
 		$uibModalInstance.dismiss("cancel");
 	}
