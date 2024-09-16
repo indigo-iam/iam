e7fe6fb46ea07fbc9b3948caf41aa67c5d8f748d
garaimanoj <manoj.garai@stfc.ac.uk>
2024-09-16 13:57:37 +0000

    Grant read access to reader for /scim/Users and /iam/scope_policies API

--- iam-login-service/src/main/java/it/infn/mw/iam/api/scim/controller/ScimUserController.java
+++ iam-login-service/src/main/java/it/infn/mw/iam/api/scim/controller/ScimUserController.java
@@ -75,7 +75,7 @@ public class ScimUserController extends ScimControllerSupport {
     return result;
   }
 
-  @PreAuthorize("#iam.hasScope('scim:read') or #iam.hasDashboardRole('ROLE_ADMIN')")
+  @PreAuthorize("#iam.hasScope('scim:read') or #iam.hasAnyDashboardRole('ROLE_ADMIN', 'ROLE_READER')")
   @RequestMapping(method = RequestMethod.GET, produces = ScimConstants.SCIM_CONTENT_TYPE)
   public MappingJacksonValue listUsers(@RequestParam(required = false) final Integer count,
       @RequestParam(required = false) final Integer startIndex,
