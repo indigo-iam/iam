1e9c200567ab07a54e2374bfa4e7f4fc40661b85
Federica Agostini <federica.agostini@cnaf.infn.it>
2025-01-28 18:13:49 +0100

    Create an approved site when using device code flow (#908)
    * Proper error message for not-allowed scopes
    * Fix AuthorizationPendingException error when client was already approved

---
+++ iam-login-service/src/main/java/it/infn/mw/iam/core/oauth/IamUserApprovalHandler.java
@@ -0,0 +1,204 @@
+/**
+ * Copyright (c) Istituto Nazionale di Fisica Nucleare (INFN). 2016-2021
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package it.infn.mw.iam.core.oauth;
+
+import static it.infn.mw.iam.core.oauth.IamOauthRequestParameters.REMEMBER_PARAMETER_KEY;
+import static org.mitre.openid.connect.request.ConnectRequestParameters.APPROVED_SITE;
+import static org.mitre.openid.connect.request.ConnectRequestParameters.PROMPT;
+import static org.mitre.openid.connect.request.ConnectRequestParameters.PROMPT_CONSENT;
+import static org.mitre.openid.connect.request.ConnectRequestParameters.PROMPT_SEPARATOR;
+import static org.springframework.security.oauth2.common.util.OAuth2Utils.USER_OAUTH_APPROVAL;
+
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.http.HttpSession;
+
+import org.mitre.oauth2.model.ClientDetailsEntity;
+import org.mitre.oauth2.service.ClientDetailsEntityService;
+import org.mitre.oauth2.service.SystemScopeService;
+import org.mitre.openid.connect.model.ApprovedSite;
+import org.mitre.openid.connect.model.WhitelistedSite;
+import org.mitre.openid.connect.service.ApprovedSiteService;
+import org.mitre.openid.connect.service.WhitelistedSiteService;
+import org.mitre.openid.connect.web.AuthenticationTimeStamper;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.provider.AuthorizationRequest;
+import org.springframework.security.oauth2.provider.approval.UserApprovalHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import com.google.common.base.Splitter;
+import com.google.common.base.Strings;
+import com.google.common.collect.Sets;
+
+@SuppressWarnings("deprecation")
+@Component("iamUserApprovalHandler")
+public class IamUserApprovalHandler implements UserApprovalHandler {
+
+  private final ClientDetailsEntityService clientDetailsService;
+  private final ApprovedSiteService approvedSiteService;
+  private final WhitelistedSiteService whitelistedSiteService;
+  private final SystemScopeService systemScopeService;
+
+  public IamUserApprovalHandler(ClientDetailsEntityService clientDetailsService,
+      ApprovedSiteService approvedSiteService, WhitelistedSiteService whitelistedSiteService,
+      SystemScopeService systemScopeService) {
+    this.clientDetailsService = clientDetailsService;
+    this.approvedSiteService = approvedSiteService;
+    this.whitelistedSiteService = whitelistedSiteService;
+    this.systemScopeService = systemScopeService;
+  }
+
+  @Override
+  public boolean isApproved(AuthorizationRequest authorizationRequest,
+      Authentication userAuthentication) {
+
+    if (authorizationRequest.isApproved()) {
+      return true;
+    } else {
+      return Boolean
+        .parseBoolean(authorizationRequest.getApprovalParameters().get(USER_OAUTH_APPROVAL));
+    }
+  }
+
+  @Override
+  public AuthorizationRequest checkForPreApproval(AuthorizationRequest authorizationRequest,
+      Authentication userAuthentication) {
+
+    String prompt = (String) authorizationRequest.getExtensions().get(PROMPT);
+    List<String> prompts = Splitter.on(PROMPT_SEPARATOR).splitToList(Strings.nullToEmpty(prompt));
+    if (prompts.contains(PROMPT_CONSENT)) {
+      return authorizationRequest;
+    }
+
+    String userId = userAuthentication.getName();
+    String clientId = authorizationRequest.getClientId();
+    Set<String> scopes = authorizationRequest.getScope();
+
+    boolean alreadyApproved = false;
+
+    Collection<ApprovedSite> aps = approvedSiteService.getByClientIdAndUserId(clientId, userId);
+
+    for (ApprovedSite ap : aps) {
+
+      if (!ap.isExpired() && systemScopeService.scopesMatch(ap.getAllowedScopes(), scopes)) {
+
+
+        ap.setAccessDate(new Date());
+        approvedSiteService.save(ap);
+
+        String apId = ap.getId().toString();
+        authorizationRequest.getExtensions().put(APPROVED_SITE, apId);
+        authorizationRequest.setApproved(true);
+        alreadyApproved = true;
+
+        setAuthTime(authorizationRequest);
+      }
+    }
+
+    if (!alreadyApproved) {
+      WhitelistedSite ws = whitelistedSiteService.getByClientId(clientId);
+      if (ws != null && systemScopeService.scopesMatch(ws.getAllowedScopes(), scopes)) {
+
+        authorizationRequest.setApproved(true);
+        setAuthTime(authorizationRequest);
+      }
+    }
+
+    return authorizationRequest;
+  }
+
+  @Override
+  public AuthorizationRequest updateAfterApproval(AuthorizationRequest authorizationRequest,
+      Authentication userAuthentication) {
+
+    String userId = userAuthentication.getName();
+    String clientId = authorizationRequest.getClientId();
+    ClientDetailsEntity client = clientDetailsService.loadClientByClientId(clientId);
+    Map<String, String> approvalParams = authorizationRequest.getApprovalParameters();
+
+    if (!Boolean.parseBoolean(approvalParams.get(USER_OAUTH_APPROVAL))) {
+      return authorizationRequest;
+    }
+
+    Set<String> requestedScopes = authorizationRequest.getScope();
+    Set<String> allowedScopes = Sets.newHashSet();
+
+    // why filtering again?
+    requestedScopes.forEach(rs -> {
+      if (systemScopeService.scopesMatch(client.getScope(), Sets.newHashSet(rs))) {
+        allowedScopes.add(rs);
+      }
+    });
+
+    boolean approved = true;
+    if (allowedScopes.isEmpty() && !requestedScopes.isEmpty()) {
+      approved = false;
+    }
+    authorizationRequest.setApproved(approved);
+
+    String remember = approvalParams.get(REMEMBER_PARAMETER_KEY);
+    if (!Strings.isNullOrEmpty(remember) && !remember.equals("none")) {
+
+      Date timeout = null;
+      if (remember.equals("one-hour")) {
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.HOUR, 1);
+        timeout = cal.getTime();
+      }
+
+      ApprovedSite newSite =
+          approvedSiteService.createApprovedSite(clientId, userId, timeout, allowedScopes);
+      String newSiteId = newSite.getId().toString();
+      authorizationRequest.getExtensions().put(APPROVED_SITE, newSiteId);
+    }
+
+    setAuthTime(authorizationRequest);
+
+    return authorizationRequest;
+
+  }
+
+  private void setAuthTime(AuthorizationRequest authorizationRequest) {
+    ServletRequestAttributes attr =
+        (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+    HttpSession session = attr.getRequest().getSession();
+    if (session != null) {
+      Date authTime = (Date) session.getAttribute(AuthenticationTimeStamper.AUTH_TIMESTAMP);
+      if (authTime != null) {
+        String authTimeString = Long.toString(authTime.getTime());
+        authorizationRequest.getExtensions()
+          .put(AuthenticationTimeStamper.AUTH_TIMESTAMP, authTimeString);
+      }
+    }
+  }
+
+  @Override
+  public Map<String, Object> getUserApprovalRequest(AuthorizationRequest authorizationRequest,
+      Authentication userAuthentication) {
+    Map<String, Object> model = new HashMap<>();
+    model.putAll(authorizationRequest.getRequestParameters());
+    return model;
+  }
+
+}
---
+++ iam-login-service/src/main/java/it/infn/mw/iam/core/oauth/IamUserApprovalUtils.java
@@ -0,0 +1,110 @@
+/**
+ * Copyright (c) Istituto Nazionale di Fisica Nucleare (INFN). 2016-2021
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package it.infn.mw.iam.core.oauth;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.mitre.oauth2.model.SystemScope;
+import org.mitre.oauth2.service.SystemScopeService;
+import org.mitre.openid.connect.model.UserInfo;
+import org.mitre.openid.connect.service.ScopeClaimTranslationService;
+import org.mitre.openid.connect.service.StatsService;
+import org.mitre.openid.connect.service.UserInfoService;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.Sets;
+import com.google.gson.JsonObject;
+
+@Component
+public class IamUserApprovalUtils {
+
+  private final SystemScopeService scopeService;
+  private final StatsService statsService;
+  private final ScopeClaimTranslationService scopeClaimTranslationService;
+  private final UserInfoService userInfoService;
+
+  public IamUserApprovalUtils(SystemScopeService scopeService, StatsService statsService,
+      ScopeClaimTranslationService scopeClaimTranslationService, UserInfoService userInfoService) {
+    this.scopeService = scopeService;
+    this.statsService = statsService;
+    this.scopeClaimTranslationService = scopeClaimTranslationService;
+    this.userInfoService = userInfoService;
+  }
+
+  public Set<String> sortScopes(Set<SystemScope> scopes) {
+
+    Set<SystemScope> sortedScopes = new LinkedHashSet<>(scopes.size());
+    Set<SystemScope> systemScopes = scopeService.getAll();
+
+    systemScopes.forEach(s -> {
+      if (scopes.contains(s)) {
+        sortedScopes.add(s);
+      }
+    });
+
+    sortedScopes.addAll(Sets.difference(scopes, systemScopes));
+
+    return scopeService.toStrings(sortedScopes);
+  }
+
+  public Map<String, Map<String, String>> claimsForScopes(Authentication authUser,
+      Set<SystemScope> scopes) {
+    UserInfo user = userInfoService.getByUsername(authUser.getName());
+    Map<String, Map<String, String>> claimsForScopes = new HashMap<>();
+    if (user != null) {
+      JsonObject userJson = user.toJson();
+
+      for (SystemScope systemScope : scopes) {
+        Map<String, String> claimValues = new HashMap<>();
+
+        Set<String> claims = scopeClaimTranslationService.getClaimsForScope(systemScope.getValue());
+        for (String claim : claims) {
+          if (userJson.has(claim) && userJson.get(claim).isJsonPrimitive()) {
+            claimValues.put(claim, userJson.get(claim).getAsString());
+          }
+        }
+
+        claimsForScopes.put(systemScope.getValue(), claimValues);
+      }
+    }
+    return claimsForScopes;
+  }
+
+  public Integer approvedSiteCount(String clientId) {
+
+    return statsService.getCountForClientId(clientId).getApprovedSiteCount();
+  }
+
+  public Boolean isSafeClient(Integer count, Date clientCreatedAt) {
+
+    Date lastWeek = new Date(System.currentTimeMillis() - (60 * 60 * 24 * 7 * 1000));
+    return count > 1 && clientCreatedAt != null && clientCreatedAt.before(lastWeek);
+  }
+
+  public String getClientContactsAsString(Set<String> clientContacts) {
+
+    if (clientContacts != null) {
+      return Joiner.on(", ").join(clientContacts);
+    }
+    return "No contacts";
+  }
+}
