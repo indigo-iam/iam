fcb23c570fa9bb80447376a013cf2f1b9610cdd7
rmiccoli <roberta.miccoli@cnaf.infn.it>
2025-02-19 12:11:31 +0100

    Show acr claim in ID token

--- iam-login-service/src/main/java/it/infn/mw/iam/core/oauth/profile/aarc/AarcJWTProfileIdTokenCustomizer.java
+++ iam-login-service/src/main/java/it/infn/mw/iam/core/oauth/profile/aarc/AarcJWTProfileIdTokenCustomizer.java
@@ -58,7 +58,7 @@ public class AarcJWTProfileIdTokenCustomizer extends BaseIdTokenCustomizer {
       .filter(ADDITIONAL_CLAIMS::contains)
       .forEach(c -> idClaims.claim(c, claimValueHelper.getClaimValueFromUserInfo(c, info)));
 
-    includeAmrClaim(request, idClaims);
+    includeAmrAndAcrClaimsIfNeeded(request, idClaims);
 
     includeLabelsInIdToken(idClaims, account);
 
