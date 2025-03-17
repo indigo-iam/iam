17d4d4fa48a7e2e3641c7495ad73c638fcf80f55
Patrick Machalet <pma@patrick-machalet-mac.dyndns.cern.ch>
2025-03-17 11:31:00 +0100

    Functional filtering with valid counter to show total search results. Minor bug when combining it with count as it currently limits the search

--- iam-login-service/src/main/java/it/infn/mw/iam/api/common/ListResponseDTO.java
+++ iam-login-service/src/main/java/it/infn/mw/iam/api/common/ListResponseDTO.java
@@ -95,6 +95,18 @@ public class ListResponseDTO<T> {
       return this;
     }
 
+    public <S> Builder<T> customSetTotalResults(Long totalResults) {
+      this.totalResults = totalResults;
+      return this;
+    }
+
+    public <S> Builder<T> customFromPage(Page<S> page, Pageable op) {
+      this.itemsPerPage = page.getNumberOfElements();
+      this.startIndex = (int) op.getOffset() + 1;
+      return this;
+    }
+
+
     public <S> Builder<T> fromPage(Page<S> page, Pageable op) {
       this.totalResults = page.getTotalElements();
       this.itemsPerPage = page.getNumberOfElements();
