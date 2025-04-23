9df5cf9d1b2c52f6e2e6a18819f442c60ddf9bf4
Berk Balcı <balciiberk@gmail.com>
2025-04-24 00:06:05 +0200

    Support DER & URL-Encoded PEM client certificates (#938)

--- iam-login-service/src/main/java/it/infn/mw/iam/authn/x509/DefaultX509AuthenticationCredentialExtractor.java
+++ iam-login-service/src/main/java/it/infn/mw/iam/authn/x509/DefaultX509AuthenticationCredentialExtractor.java
@@ -86,7 +86,7 @@ public class DefaultX509AuthenticationCredentialExtractor
   private X509CertificateVerificationResult parseVerifyHeader(HttpServletRequest request) {
     String verifyHeaderContent = request.getHeader(VERIFY.header);
 
-    if ("SUCCESS".equals(verifyHeaderContent)) {
+    if ("SUCCESS".equals(verifyHeaderContent) || "X509_V_OK".equals(verifyHeaderContent)) {
       return X509CertificateVerificationResult.success();
     }
 
@@ -110,7 +110,7 @@ public class DefaultX509AuthenticationCredentialExtractor
   public Optional<IamX509AuthenticationCredential> extractX509Credential(
       HttpServletRequest request) {
 
-    String clientCertHeaderContent = getHeader(request,  CLIENT_CERT);
+    String clientCertHeaderContent = getHeader(request, CLIENT_CERT);
     
     if (Strings.isNullOrEmpty(clientCertHeaderContent)) {
       LOG.debug("{} null or empty", CLIENT_CERT.header);
@@ -119,10 +119,8 @@ public class DefaultX509AuthenticationCredentialExtractor
 
     headerNamesSanityChecks(request);
 
-    String pemCertificateString = clientCertHeaderContent.replace('\t', '\n');
-    
     X509CertificateChainParsingResult chain =
-        certChainParser.parseChainFromString(pemCertificateString);
+        certChainParser.parseChainFromString(clientCertHeaderContent);
     
     IamX509AuthenticationCredential.Builder credBuilder =
         new IamX509AuthenticationCredential.Builder();
