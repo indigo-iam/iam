32c13d1d876575786d60d2d675ad2045b3ae44ee
Patrick Machalet <patrick.machalet@cern.ch>
2025-06-10 11:54:50 +0200

    Changed environment variable, now 3 options, required, optional, and off. Only required and off is functioning at the moment

--- iam-login-service/src/main/java/it/infn/mw/iam/authn/x509/IamX509PreauthenticationProcessingFilter.java
+++ iam-login-service/src/main/java/it/infn/mw/iam/authn/x509/IamX509PreauthenticationProcessingFilter.java
@@ -36,6 +36,7 @@ import it.infn.mw.iam.config.IamProperties;
 import it.infn.mw.iam.persistence.model.IamAccount;
 import it.infn.mw.iam.persistence.model.IamX509Certificate;
 import it.infn.mw.iam.persistence.repository.IamX509CertificateRepository;
+import it.infn.mw.iam.config.IamProperties.RequireCertificateOption;
 
 public class IamX509PreauthenticationProcessingFilter
     extends AbstractPreAuthenticatedProcessingFilter {
