c34adf777eaddc15c6e57d5ffd9990d7b67bb51b
rmiccoli <roberta.miccoli@cnaf.infn.it>
2025-07-29 11:00:57 +0200

    Fix trust chain resolution steps

--- iam-login-service/src/main/java/it/infn/mw/iam/core/oidc/TrustChainService.java
+++ iam-login-service/src/main/java/it/infn/mw/iam/core/oidc/TrustChainService.java
@@ -26,6 +26,7 @@ import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriUtils;
@@ -38,6 +39,7 @@ import com.nimbusds.openid.connect.sdk.federation.entities.EntityStatement;
 import com.nimbusds.openid.connect.sdk.federation.trust.TrustChain;
 
 @Service
+@Profile("openid-federation")
 public class TrustChainService {
 
   public static final Logger LOG = LoggerFactory.getLogger(TrustChainService.class);
@@ -80,19 +82,30 @@ public class TrustChainService {
     // Recursive Trust Chain construction
     List<EntityStatement> chain = buildChain(ec, new HashSet<>());
 
-    // Check that Trust Chain ends with a trusted TA
-    EntityStatement ta = chain.get(chain.size() - 1);
-    if (!trustAnchorRepository.isTrusted(ta.getEntityID().getValue())) {
-      throw new TrustChainException("No trusted Trust Anchor found");
+    // Validate base claims
+    for (EntityStatement es : chain) {
+      validateClaims(es);
     }
 
-    // Preliminary checks on claims and Trust Chain structure
-    validateClaimsAndStructure(chain);
+    // Build TrustChain (check iss/sub chain)
+    TrustChain trustChain;
+    try {
+      trustChain = new TrustChain(ec, chain);
+    } catch (IllegalArgumentException e) {
+      throw new TrustChainException("Invalid trust chain structure: " + e.getMessage(), e);
+    }
+
+    // Check that Trust Chain ends with a trusted TA
+    EntityID taId = trustChain.getTrustAnchorEntityID();
+    if (!trustAnchorRepository.isTrusted(taId.getValue())) {
+      throw new TrustChainException("No trusted Trust Anchor found: " + taId.getValue());
+    }
 
     // Cascading signatures (from TA to RP)
-    validateSignatures(chain);
+    EntityStatement ta = chain.get(chain.size() - 1);
+    trustChain.verifySignatures(ta.getClaimsSet().getJWKSet());
 
-    return new TrustChain(ec, chain);
+    return trustChain;
   }
 
   /**
@@ -200,68 +213,6 @@ public class TrustChainService {
 
     if (exp.before(now)) {
       throw new TrustChainException("Entity Statement is expired: " + exp);
-    }
-  }
-
-  /**
-   * Validation of all claims and issuer-subject consistency for the whole Trust Chain
-   */
-  private void validateClaimsAndStructure(List<EntityStatement> chain) throws TrustChainException {
-    for (EntityStatement es : chain) {
-      validateClaims(es);
-    }
-
-    // RP: iss == sub
-    EntityStatement rpEC = chain.get(0);
-    if (!rpEC.getClaimsSet().isSelfStatement()) {
-      throw new TrustChainException(
-          "Entity Configuration of RP must be self-issued self-issued (iss == sub)");
-    }
-
-    // TA: iss == sub
-    EntityStatement trustAnchor = chain.get(chain.size() - 1);
-    if (!trustAnchor.getClaimsSet().isSelfStatement()) {
-      throw new TrustChainException(
-          "Entity Configuration of Trust Anchor must be self-issued (iss == sub)");
-    }
-
-    // Iss-sub matching for each pair in the Trust Chain
-    for (int i = 0; i < chain.size() - 1; i++) {
-      EntityStatement current = chain.get(i);
-      EntityStatement issuer = chain.get(i + 1);
-
-      if (!current.getClaimsSet()
-        .getIssuer()
-        .getValue()
-        .equals(issuer.getClaimsSet().getSubject().getValue())) {
-        throw new TrustChainException("Issuer/Subject mismatch between chain elements");
-      }
-    }
-  }
-
-  /**
-   * Check the signatures of the entire Trust Chain starting from the Trust Anchor
-   * 
-   * @throws JOSEException
-   * @throws BadJOSEException
-   */
-  private void validateSignatures(List<EntityStatement> chain)
-      throws TrustChainException, BadJOSEException, JOSEException {
-
-    // Signature of the RP with its JWKS (self-signed)
-    EntityStatement rpEC = chain.get(0);
-    rpEC.verifySignatureOfSelfStatement();
-
-    // Signature of the Trust Anchor with its JWKS (self-signed)
-    EntityStatement trustAnchor = chain.get(chain.size() - 1);
-    trustAnchor.verifySignatureOfSelfStatement();
-
-    // Signing each ES with the public key of the next issuer
-    for (int i = chain.size() - 2; i >= 0; i--) {
-      EntityStatement current = chain.get(i);
-      EntityStatement issuer = chain.get(i + 1);
-
-      current.verifySignature(issuer.getClaimsSet().getJWKSet());
     }
   }
 }
