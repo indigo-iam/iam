be0612bb2e591e2d784126cf46946bf238426986
Mia Mutadich <131201068+mmutadich@users.noreply.github.com>
2025-09-15 09:57:53 +0200

    Changes

--- iam-login-service/src/test/java/it/infn/mw/iam/test/oauth/TokenExchangeDisableUpscopingTests.java
+++ iam-login-service/src/test/java/it/infn/mw/iam/test/oauth/TokenExchangeDisableUpscopingTests.java
@@ -44,7 +44,7 @@ import it.infn.mw.iam.test.util.annotation.IamMockMvcIntegrationTest;
 @SuppressWarnings("deprecation")
 @RunWith(SpringRunner.class)
 @IamMockMvcIntegrationTest
-@TestPropertySource(properties = {"iam.jwt-profile.token-exchange-upscoping-enabled=true",
+@TestPropertySource(properties = {"iam.jwt-profile.token-exchange-upscoping-enabled=false",
         "iam.access_token.include_scope=true"})
 public class TokenExchangeDisableUpscopingTests extends EndpointsTestUtils {
 
