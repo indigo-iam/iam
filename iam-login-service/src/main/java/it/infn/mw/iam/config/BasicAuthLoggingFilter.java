c86fb050bb39cf1ed657f016e3a888dcc051a3b5
rmiccoli <roberta.miccoli@cnaf.infn.it>
2025-11-27 18:23:56 +0100

    Add filter to log basic auth header

---
+++ iam-login-service/src/main/java/it/infn/mw/iam/config/BasicAuthLoggingFilter.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) Istituto Nazionale di Fisica Nucleare (INFN). 2016-2021
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package it.infn.mw.iam.config;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class BasicAuthLoggingFilter extends OncePerRequestFilter {
+
+  private static final Logger log = LoggerFactory.getLogger(BasicAuthLoggingFilter.class);
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+      FilterChain filterChain) throws ServletException, IOException {
+    if (log.isDebugEnabled()) {
+
+      String auth = request.getHeader("Authorization");
+
+      if (auth != null && auth.startsWith("Basic ")) {
+        String base64 = auth.substring("Basic ".length());
+        log.debug("Received Basic Authorization header (Base64 only): {}", base64);
+      }
+      filterChain.doFilter(request, response);
+    }
+  }
+}
