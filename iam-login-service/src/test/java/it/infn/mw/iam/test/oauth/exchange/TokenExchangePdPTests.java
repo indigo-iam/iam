3110df09cd2e9cf8b2682be584fd17e7c0585829
Patrick Machalet <patrick.machalet@cern.ch>
2026-01-19 14:32:55 +0100

    Improving code coverage

--- iam-login-service/src/test/java/it/infn/mw/iam/test/oauth/exchange/TokenExchangePdPTests.java
+++ iam-login-service/src/test/java/it/infn/mw/iam/test/oauth/exchange/TokenExchangePdPTests.java
@@ -25,13 +25,19 @@ import static java.util.Collections.emptyMap;
 import static java.util.stream.Collectors.toSet;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.lenient;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.velocity.exception.ParseErrorException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -40,9 +46,14 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
 import org.springframework.security.oauth2.provider.ClientDetails;
 import org.springframework.security.oauth2.provider.TokenRequest;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import it.infn.mw.iam.core.IamTokenService;
 import it.infn.mw.iam.core.oauth.exchange.DefaultTokenExchangePdp;
 import it.infn.mw.iam.core.oauth.exchange.TokenExchangePdpResult;
@@ -79,6 +90,8 @@ class TokenExchangePdPTests extends TokenExchangePdpTestSupport {
   @Mock
   IamTokenService tokenService;
 
+  private ListAppender<ILoggingEvent> logCaptor;
+
   // These are not tests of the Upscoping, thus the scopes extracted from the token is not what is
   // in question. Therefore, it should always make the static scopematchers for the token.
   class TestableDefaultTokenExchangePdp extends DefaultTokenExchangePdp {
@@ -102,6 +115,14 @@ class TokenExchangePdPTests extends TokenExchangePdpTestSupport {
   static final Set<ScopeMatcher> FIXED_MATCHERS =
       Set.of(StringEqualsScopeMatcher.stringEqualsMatcher("s1"),
           StringEqualsScopeMatcher.stringEqualsMatcher("s2"));
+
+  private ListAppender<ILoggingEvent> attachLogCaptor(Class<?> clazz) {
+    Logger logger = (Logger) org.slf4j.LoggerFactory.getLogger(clazz);
+    ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+    listAppender.start();
+    logger.addAppender(listAppender);
+    return listAppender;
+  }
 
   private TokenRequest buildTokenRequest() {
     return new TokenRequest(emptyMap(), "destination", Collections.emptySet(),
@@ -386,5 +407,51 @@ class TokenExchangePdPTests extends TokenExchangePdpTestSupport {
 
     assertThat(result.decision(), is(Decision.DENY));
     assertThat(result.message().get(), is("Subject token not present in request"));
+  }
+
+
+  // This test is more for completion, an invalid subject token shouldn't be possible at this point.
+  @Test
+  void extractScopesFromTokenInvalid() throws Exception {
+
+
+    DefaultTokenExchangePdp pdp =
+        new DefaultTokenExchangePdp(repo, scopeMatchersRegistry, tokenService);
+
+    // Method is not meant to be visible passed the package, so will have to change visibility
+    Method method =
+        DefaultTokenExchangePdp.class.getDeclaredMethod("extractScopesFromToken", String.class);
+    method.setAccessible(true);
+
+
+    String invalidJwt = "this-is-not-a-jwt";
+
+    Mockito.when(tokenService.readAccessToken(invalidJwt))
+      .thenThrow(new InvalidTokenException("Access Token not found"));
+
+    logCaptor = attachLogCaptor(DefaultTokenExchangePdp.class);
+
+    // Parse Error expected after both attempts fail
+    try {
+      method.invoke(pdp, invalidJwt);
+      fail("Expected ParseErrorException to be thrown");
+    } catch (InvocationTargetException e) {
+
+      // Needing to unwrap the error
+      Throwable cause = e.getCause();
+
+      assertThat(cause, instanceOf(ParseErrorException.class));
+      assertThat(cause.getMessage(),
+          is("Error whilst extracting scopes from the token and failed token introspection"));
+    }
+
+    // Catching that it also fails the first introspection due to an error
+    boolean found = logCaptor.list.stream()
+      .anyMatch(event -> event.getLevel() == Level.WARN
+          && event.getFormattedMessage()
+            .contains("JWT parsing failed, attempting token introspection")
+          && event.getThrowableProxy().getClassName().equals("java.text.ParseException"));
+
+    assertTrue(found);
   }
 }
