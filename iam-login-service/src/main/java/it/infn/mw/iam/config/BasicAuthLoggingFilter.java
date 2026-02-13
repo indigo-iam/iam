eeb5b98f26065183a04769883e195c995f660d21
rmiccoli <roberta.miccoli@cnaf.infn.it>
2026-02-13 12:01:29 +0100

    Enable basic auth filter only for oidfed
    for debugging purposes

--- iam-login-service/src/main/java/it/infn/mw/iam/config/BasicAuthLoggingFilter.java
+++ iam-login-service/src/main/java/it/infn/mw/iam/config/BasicAuthLoggingFilter.java
@@ -25,10 +25,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
+@Profile("openid-federation")
 public class BasicAuthLoggingFilter extends OncePerRequestFilter {
 
   private static final Logger log = LoggerFactory.getLogger(BasicAuthLoggingFilter.class);
