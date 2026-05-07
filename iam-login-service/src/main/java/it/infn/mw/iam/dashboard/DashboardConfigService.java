930d11b89ff672b010c04246864ab74baa6e1820
enricovianello <maverick17586@gmail.com>
2026-05-07 18:42:19 +0200

    Change default Dashboard client name and description

--- iam-login-service/src/main/java/it/infn/mw/iam/dashboard/DashboardConfigService.java
+++ iam-login-service/src/main/java/it/infn/mw/iam/dashboard/DashboardConfigService.java
@@ -43,6 +43,8 @@ public class DashboardConfigService {
 
   private static final Logger LOG = LoggerFactory.getLogger(DashboardConfigService.class);
 
+  public static final String DASHBOARD_CLIENT_NAME = "The INDIGO IAM Dashboard";
+  public static final String DASHBOARD_CLIENT_DESCRIPTION = "Creating trust, one token at a time";
   public static final String DASHBOARD_CALLBACK = "/ui/api/auth/oauth2/callback/indigo-iam";
   public static final Set<String> DASHBOARD_SCOPES =
       Set.of(SystemScopeService.OPENID_SCOPE, SystemScopeService.OFFLINE_ACCESS, "email", "profile",
@@ -130,7 +132,8 @@ public class DashboardConfigService {
     RegisteredClientDTO client = new RegisteredClientDTO();
     client.setScope(DASHBOARD_SCOPES);
     client.setClientId(clientId);
-    client.setClientName("dashboard");
+    client.setClientName(DASHBOARD_CLIENT_NAME);
+    client.setClientDescription(DASHBOARD_CLIENT_DESCRIPTION);
     client.setClientSecret(secret);
     client.setTokenEndpointAuthMethod(TokenEndpointAuthenticationMethod.client_secret_basic);
     client.setAccessTokenValiditySeconds(3600);
