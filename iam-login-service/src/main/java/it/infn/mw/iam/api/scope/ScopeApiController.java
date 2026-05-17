5096563bbbb6a88ca7910a7a32aa73715ca0e9ea
enricovianello <maverick17586@gmail.com>
2026-05-17 23:09:21 +0200

    Add alias for scopes API base URL

--- iam-login-service/src/main/java/it/infn/mw/iam/api/scope/ScopeApiController.java
+++ iam-login-service/src/main/java/it/infn/mw/iam/api/scope/ScopeApiController.java
@@ -32,7 +32,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api/scopes")
+@RequestMapping(value = {"/api/scopes", "/iam/api/scopes"})
 public class ScopeApiController {
 
   private final SystemScopeService scopeService;
