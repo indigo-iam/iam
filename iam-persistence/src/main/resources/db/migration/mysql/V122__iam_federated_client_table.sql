6b72cd865a9c8796771c16027c19a2e03f21b1c8
rmiccoli <roberta.miccoli@cnaf.infn.it>
2026-07-01 10:47:03 +0200

    Fix db migrations
    and add primary and foreign keys

--- iam-persistence/src/main/resources/db/migration/mysql/V122__iam_federated_client_table.sql
+++ iam-persistence/src/main/resources/db/migration/mysql/V122__iam_federated_client_table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS iam_federated_client (
 	id BIGINT AUTO_INCREMENT PRIMARY KEY,
 	
-	client_id VARCHAR(256),
+	client_id VARCHAR(256) NOT NULL,
 	client_secret TEXT,
 	
-	client_name VARCHAR(256),
+	client_name VARCHAR(256) NOT NULL,
 	token_endpoint_auth_method VARCHAR(256),
 
 	jwks_uri TEXT,
@@ -20,21 +20,29 @@ CREATE TABLE IF NOT EXISTS iam_federated_client (
 );
 
 CREATE TABLE IF NOT EXISTS iam_federated_client_redirect_uri (
-    owner_id VARCHAR(256),
-    redirect_uri VARCHAR(2048)
+    owner_id BIGINT NOT NULL,
+    redirect_uri VARCHAR(2048) NOT NULL,
+    CONSTRAINT pk_iam_federated_client_redirect_uri PRIMARY KEY (owner_id, redirect_uri),
+    CONSTRAINT fk_iam_federated_client_redirect_uri FOREIGN KEY (owner_id) REFERENCES iam_federated_client (id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS iam_federated_client_grant_type (
-    owner_id VARCHAR(256),
-    grant_type VARCHAR(2000)
+    owner_id BIGINT NOT NULL,
+    grant_type VARCHAR(2000) NOT NULL,
+    CONSTRAINT pk_iam_federated_client_grant_type PRIMARY KEY (owner_id, grant_type),
+    CONSTRAINT fk_iam_federated_client_grant_type FOREIGN KEY (owner_id) REFERENCES iam_federated_client (id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS iam_federated_client_response_type (
-    owner_id VARCHAR(256),
-    response_type VARCHAR(2000)
+    owner_id BIGINT NOT NULL,
+    response_type VARCHAR(2000) NOT NULL,
+    CONSTRAINT pk_iam_federated_client_response_type PRIMARY KEY (owner_id, response_type),
+    CONSTRAINT fk_iam_federated_client_response_type FOREIGN KEY (owner_id) REFERENCES iam_federated_client (id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS iam_federated_client_scope (
-    owner_id VARCHAR(256),
-    scope VARCHAR(2048)
+    owner_id BIGINT NOT NULL,
+    scope VARCHAR(2048) NOT NULL,
+    CONSTRAINT pk_iam_federated_client_scope PRIMARY KEY (owner_id, scope),
+    CONSTRAINT fk_iam_federated_client_scope FOREIGN KEY (owner_id) REFERENCES iam_federated_client (id) ON DELETE CASCADE
 );
