fb194f0eb5fec84dd09e0b46233240acc13af99b
rmiccoli <roberta.miccoli@cnaf.infn.it>
2026-07-03 17:39:20 +0200

    Drop composite primare keys
    from federated client collection tables
    to avoid MySQL key length limitations
    on large VARCHAR columns.

--- iam-persistence/src/main/resources/db/migration/mysql/V122__iam_federated_client_table.sql
+++ iam-persistence/src/main/resources/db/migration/mysql/V122__iam_federated_client_table.sql
@@ -22,27 +22,23 @@ CREATE TABLE IF NOT EXISTS iam_federated_client (
 CREATE TABLE IF NOT EXISTS iam_federated_client_redirect_uri (
     owner_id BIGINT NOT NULL,
     redirect_uri VARCHAR(2048) NOT NULL,
-    CONSTRAINT pk_iam_federated_client_redirect_uri PRIMARY KEY (owner_id, redirect_uri),
     CONSTRAINT fk_iam_federated_client_redirect_uri FOREIGN KEY (owner_id) REFERENCES iam_federated_client (id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS iam_federated_client_grant_type (
     owner_id BIGINT NOT NULL,
     grant_type VARCHAR(2000) NOT NULL,
-    CONSTRAINT pk_iam_federated_client_grant_type PRIMARY KEY (owner_id, grant_type),
     CONSTRAINT fk_iam_federated_client_grant_type FOREIGN KEY (owner_id) REFERENCES iam_federated_client (id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS iam_federated_client_response_type (
     owner_id BIGINT NOT NULL,
     response_type VARCHAR(2000) NOT NULL,
-    CONSTRAINT pk_iam_federated_client_response_type PRIMARY KEY (owner_id, response_type),
     CONSTRAINT fk_iam_federated_client_response_type FOREIGN KEY (owner_id) REFERENCES iam_federated_client (id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS iam_federated_client_scope (
     owner_id BIGINT NOT NULL,
     scope VARCHAR(2048) NOT NULL,
-    CONSTRAINT pk_iam_federated_client_scope PRIMARY KEY (owner_id, scope),
     CONSTRAINT fk_iam_federated_client_scope FOREIGN KEY (owner_id) REFERENCES iam_federated_client (id) ON DELETE CASCADE
 );
