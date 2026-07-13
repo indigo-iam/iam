febe80f2b359834e0a6ecf302538de1f10170281
rmiccoli <roberta.miccoli@cnaf.infn.it>
2026-07-13 18:10:13 +0200

    Clean test

--- iam-login-service/src/test/java/it/infn/mw/iam/test/oauth/ClientCredentialsDecodingTests.java
+++ iam-login-service/src/test/java/it/infn/mw/iam/test/oauth/ClientCredentialsDecodingTests.java
@@ -27,7 +27,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -35,13 +34,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nimbusds.jwt.SignedJWT;
 
-import it.infn.mw.iam.IamLoginService;
-import it.infn.mw.iam.test.config.ClockConfig;
-import it.infn.mw.iam.test.core.CoreControllerTestSupport;
-
-@SpringBootTest(
-    classes = {IamLoginService.class, CoreControllerTestSupport.class, ClockConfig.class},
-    webEnvironment = WebEnvironment.MOCK)
+@SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
 class ClientCredentialsDecodingTests {
