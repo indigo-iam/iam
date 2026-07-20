e655fc0b6d82950d092ff594d57b2dc71d84fb4e
Federica Agostini <federica.agostini@cnaf.infn.it>
2026-07-20 16:36:42 +0200

    Add property for client secret encoder key

--- iam-login-service/src/main/java/it/infn/mw/iam/config/IamProperties.java
+++ iam-login-service/src/main/java/it/infn/mw/iam/config/IamProperties.java
@@ -601,6 +601,7 @@ public class IamProperties {
 
   public static class ClientProperties {
     private boolean trackLastUsed = false;
+    private String secretEncoderKey;
 
     public boolean isTrackLastUsed() {
       return trackLastUsed;
@@ -608,6 +609,14 @@ public class IamProperties {
 
     public void setTrackLastUsed(boolean trackLastUsed) {
       this.trackLastUsed = trackLastUsed;
+    }
+
+    public String getSecretEncoderKey() {
+      return secretEncoderKey;
+    }
+
+    public void setSecretEncoderKey(String secretEncoderKey) {
+      this.secretEncoderKey = secretEncoderKey;
     }
   }
 
