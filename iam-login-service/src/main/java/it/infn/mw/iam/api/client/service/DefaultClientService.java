c7017987f34e0c185f0accca922f688a4e02e15b
Patrick Machalet <s203932@dtu.dk>
2026-09-24 10:46:52 +0200

    Temporary logging for debugging

--- iam-login-service/src/main/java/it/infn/mw/iam/api/client/service/DefaultClientService.java
+++ iam-login-service/src/main/java/it/infn/mw/iam/api/client/service/DefaultClientService.java
@@ -22,6 +22,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.Supplier;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
@@ -32,6 +34,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import it.infn.mw.iam.audit.events.client.ClientCreatedEvent;
+import it.infn.mw.iam.authn.AuthenticationSuccessHandlerHelper;
 import it.infn.mw.iam.core.oauth.scope.matchers.DefaultScopeMatcherRegistry;
 import it.infn.mw.iam.persistence.model.ClientDetailsEntity;
 import it.infn.mw.iam.persistence.model.ClientLastUsedEntity;
@@ -44,6 +47,9 @@ import it.infn.mw.iam.persistence.repository.client.IamClientRepository;
 @Service
 @Transactional
 public class DefaultClientService implements ClientService {
+
+  private static final Logger logger =
+      LoggerFactory.getLogger(DefaultClientService.class);
 
   private final Clock clock;
 
@@ -115,6 +121,7 @@ public class DefaultClientService implements ClientService {
   @Cacheable(cacheNames = "Clients", key = "#clientId")
   @Override
   public Optional<ClientDetailsEntity> findClientByClientId(String clientId) {
+    logger.info("CACHE MISS / METHOD EXECUTED: {}", clientId);
     return clientRepo.findByClientId(clientId);
   }
 
